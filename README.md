a9765a1a6a3aa797be552afbae9843becfcde4e8
Juan Colacelli <spam@colacelli.com>
2018-11-08 10:47:21 -0300

    First commit :fireworks:

---
+++ README.md
@@ -0,0 +1,20 @@
+# GNU Librebot
+
+## Dependencies
+* **irclib**: https://gitlab.com/jic/irclib
+* **ircbot**: https://gitlab.com/jic/ircbot
+* **revo**: http://www.reta-vortaro.de/tgz/
+
+### Configuration
+#### librebot.properties
+You need to rename librebot.properties.sample as librebot.properties
+
+* **SERVER**: IRC server hostname
+* **PORT**: IRC server port
+* **SECURE**: Activate/Deactivate SSL
+* **PASSWORD**: IRC server password
+* **LOGIN**: IRCBot login
+* **NICK**: IRCBot nick
+* **NICKSERV_PASSWORD**: NickServ authentication password
+* **CHANNELS**: Channels to auto-join (comma separated without spaces)
+* **REVO_PATH**: Unzipped revo path
---
+++ src/com/colacelli/librebot/Librebot.java
@@ -0,0 +1,93 @@
+package com.colacelli.librebot;
+
+import com.colacelli.ircbot.IRCBot;
+import com.colacelli.ircbot.plugins.autojoin.AutoJoinPlugin;
+import com.colacelli.ircbot.plugins.autoreconnect.AutoReconnectPlugin;
+import com.colacelli.ircbot.plugins.esperantotranslator.EsperantoTranslatorPlugin;
+import com.colacelli.ircbot.plugins.help.HelpPlugin;
+import com.colacelli.ircbot.plugins.nickserv.NickServPlugin;
+import com.colacelli.ircbot.plugins.operator.OperatorPlugin;
+import com.colacelli.ircbot.plugins.rejoinonkick.RejoinOnKickPlugin;
+import com.colacelli.ircbot.plugins.uptime.UptimePlugin;
+import com.colacelli.ircbot.plugins.websitetitle.WebsiteTitlePlugin;
+import com.colacelli.irclib.actors.Channel;
+import com.colacelli.irclib.actors.User;
+import com.colacelli.irclib.connection.Server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class Librebot {
+    private static final String PROPERTIES_FILE = "com/colacelli/librebot/librebot.properties";
+
+    public static void main(String[] args) {
+        Properties properties = loadProperties(PROPERTIES_FILE);
+
+        IRCBot bot = new IRCBot();
+
+        addPlugins(bot, properties);
+
+        bot.connect(
+                buildServer(properties),
+                buildUser(properties)
+        );
+    }
+
+    private static Properties loadProperties(String propertiesFile) {
+        InputStream inputStream;
+        inputStream = ClassLoader.getSystemResourceAsStream(propertiesFile);
+
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return properties;
+    }
+
+    private static User buildUser(Properties properties) {
+        User.Builder userBuilder = new User.Builder();
+        userBuilder
+                .setNick(properties.getProperty("NICK"))
+                .setLogin(properties.getProperty("LOGIN"));
+
+        return userBuilder.build();
+    }
+
+    private static Server buildServer(Properties properties) {
+        Server.Builder serverBuilder = new Server.Builder();
+        serverBuilder
+                .setHostname(properties.getProperty("SERVER"))
+                .setPort(Integer.parseInt(properties.getProperty("PORT")))
+                .setSecure(Boolean.parseBoolean(properties.getProperty("SECURE")))
+                .setPassword(properties.getProperty("PASSWORD"));
+
+        return serverBuilder.build();
+    }
+
+    private static void addPlugins(IRCBot bot, Properties properties) {
+        ArrayList<Channel> channels = new ArrayList<>();
+        for (String channel : properties.getProperty("CHANNELS").split(",")) {
+            channels.add(new Channel(channel));
+        }
+
+        // Behaviour
+        bot.addPlugin(new AutoReconnectPlugin());
+        bot.addPlugin(new AutoJoinPlugin(channels));
+        bot.addPlugin(new RejoinOnKickPlugin());
+        bot.addPlugin(new NickServPlugin(properties.getProperty("NICKSERV_PASSWORD")));
+        bot.addPlugin(new WebsiteTitlePlugin());
+
+        // Commands
+        bot.addPlugin(new UptimePlugin());
+        bot.addPlugin(new OperatorPlugin());
+        bot.addPlugin(new EsperantoTranslatorPlugin(properties.getProperty("REVO_PATH")));
+
+        // Help
+        bot.addPlugin(new HelpPlugin());
+    }
+}
