56c1815ae38afcce94884cd3bbaeb34145d41098
Juan Colacelli <spam@colacelli.com>
2018-12-22 08:22:39 -0300

    Adding graddle support.

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    maven
+    kotlin("jvm") version "1.3.11"
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+
+    compile("com.gitlab.jic:ircbot:gradle-SNAPSHOT")
+}
+
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
