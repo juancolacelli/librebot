a50ddadf982eccdb40dec862f0e07eb5c45b8323
Juan Colacelli <spam@colacelli.com>
2019-01-08 14:34:10 -0300

    GNU IRC Bot library updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
-    compile("com.gitlab.jic:ircbot:0.2.3") {
+    compile("com.gitlab.jic:ircbot:0.2.4") {
         isChanging = true
     }
 }
